d5c8773413ed5b264ea2c91b7a5d73749bf5b77c
enoch <ifemideenoch96@gamil.com>
2023-10-16 08:06:34 -0700

    nw

---
+++ index.js
@@ -0,0 +1,36 @@
+// this is a calculation
+let x =2000
+let y =300
+let total = x-y
+console.log(total)
+
+// area of rectangle
+
+let lenght = 20
+let breadth = 10
+const areatriangle = 0.5*breadth*lenght
+console.log("area  triangle="+ areatriangle)
+// circle
+let pi =3.14
+let r =44
+const circumference = 2*pi*r
+console.log("circumference="+ circumference )
+
+// function of multiplication
+function multiplication(x,y){
+  let multip = x*y
+  return multip
+}
+console.log(multiplication(5,5))
+// function of substraction
+function substraction(x,y){
+  let substract  = x-y
+  return substract
+}
+console.log(substraction(15,5))
+// function of division
+function division(x,y){
+  let divide  = x/y
+  return divide
+}
+console.log(division(15,5))
